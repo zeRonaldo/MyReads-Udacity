8fe3d6fa1701dca2e2f64221705e36e5ddd84641
José Ronaldo Souza Junior <ronaldo.souza@faciltecnologia.com.br>
2018-11-16 15:41:33 -0300

    Book-item refatorado

--- projeto-aleatorio/src/templates/Book.js
+++ projeto-aleatorio/src/templates/Book.js
@@ -1,22 +1,68 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {Col, Card, CardTitle, Chip, Icon} from 'react-materialize'
+import Row from 'react-materialize/lib/Row';
 /* eslint-disable no-unused-expressions */
 
 class Book extends Component{
-    
- 
+    printRating(rate){
+        let stars ;
+        let i = 0;
+        
+        if (rate % 1 != 0){
+            for(i ; i == rate; i ++ ){
+                stars += <Icon>star</Icon>;
+            }
+            stars += <Icon>half_star</Icon>;
+            return stars;
+        }else{
+            for(i ; i == rate; i ++ ){
+                stars += <Icon>star</Icon>;
+            }
+            return stars;
+        }
+    }
+    printAuthors (authors){
+        let authorChip;
+        authors.forEach(author => {
+            authorChip = <Chip>{authors}</Chip>
+        });
+        return authorChip;
+        
+    }
     render() {
        
         let comp = this.props.books.map( (book) => {
-            return <li key={book.id} className="book-mini">
-                        <img src={book.imageLinks.thumbnail} className="cover"></img>
-                        <h4>{book.title.substr(0, 30)}</h4>
-                        <h5>{book.authors[0]} </h5>
-                        <Link to={'/books/'+ book.id}>
-                            + Detalhes
-                        </Link>
+            let content = <Row>
+                <Row>
+                    {this.printRating(book.averageRating)}
+                </Row>
+                <Row className="book-authors">
+                    Author: {this.printAuthors(book.authors)}
+                </Row>
+                <Row className="book-genre">
+                    Genre: <Chip>{book.categories}</Chip>
+                </Row>
+
+             </Row>;
+
+           return <Col s={12} m={4} l={3}>
+                <Card header={<CardTitle reveal image={book.imageLinks.thumbnail} waves='light'/>}
+                    title={book.title.substr(0,30)}
+                    reveal={content}>
+                    <p><Link to={'/books/' + book.id} >More details</Link></p>
+                </Card>
+            </Col>
+           
+            // return <li key={book.id} className="book-mini">
+            //             <img src={book.imageLinks.thumbnail} className="cover"></img>
+            //             <h4>{book.title.substr(0, 30)}</h4>
+            //             <h5>{book.authors[0]} </h5>
+            //             <Link to={'/books/'+ book.id}>
+            //                 + Detalhes
+            //             </Link>
                         
-                    </li>
+            //         </li>
         })  
         return(
             
